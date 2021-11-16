c0b3edcf34c9dec5fd6db57e389b1140a151f8dc
Yann Thomas LE MOIGNE <ylemoigne@externe.generali.fr>
2021-11-16 22:05:04 +0100

    Création du jeux à proprement dit

--- game/Controllers/ViewController.swift
+++ game/Controllers/ViewController.swift
@@ -27,7 +27,63 @@ class ViewController: UIViewController {
         let name = Notification.Name(rawValue: "QuestionsLoaded")
         NotificationCenter.default.addObserver(self, selector: #selector(questionLoaded), name: name, object: nil)
         
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragQuestionView(_:)))
+        questionView.addGestureRecognizer(panGestureRecognizer)
+        
         startNewGame()
+    }
+    
+    @objc private func dragQuestionView(_ sender: UIPanGestureRecognizer) {
+        if game.state == .over {
+            return
+        }
+        switch sender.state {
+        case .began, .changed:
+            transformQuestionViewWith(gesture: sender)
+        case .ended, .cancelled:
+            answerQuestion()
+        default:
+            break
+        }
+    }
+    
+    private func transformQuestionViewWith(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: questionView)
+        let translationTransform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        let translationPercent = translation.x/(UIScreen.main.bounds.width / 2)
+        let rotationAngle = (CGFloat.pi / 6) * translationPercent
+        let rotationTransform = CGAffineTransform(rotationAngle: rotationAngle)
+        
+        let transform = translationTransform.concatenating(rotationTransform)
+        questionView.transform = transform
+        
+        if translation.x < 0 {
+            questionView.style = .incorrect
+        } else if translation.x > 0 {
+            questionView.style = .correct
+        } else {
+            questionView.style = .standard
+        }
+    }
+
+    private func answerQuestion() {
+        switch questionView.style {
+            case .correct:
+                game.answerCurrentQuestion(with: true)
+            case .incorrect:
+                game.answerCurrentQuestion(with: false)
+            case .standard:
+                break
+        }
+        
+        questionView.transform = .identity
+        questionView.style = .standard
+        scoreLabel.text = "\(game.score) / 10"
+        if game.state == .ongoing {
+            questionView.title = game.currentQuestion.title
+        } else {
+            questionView.title = "Finished"
+        }
     }
     
     @objc private func questionLoaded() {
